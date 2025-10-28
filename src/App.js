706ead372f824cd41a6444d3e1bc3204c127c231
1711Jeetu <2022.jeetu.mamtora@ves.ac.in>
2025-10-29 00:53:37 +0530

    Feat : Added Wishlist Route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import { Footer } from "./components/Layout/Footer";
 import { Header } from "./components/Layout/Header";
 import { Approutes } from "./routes/Approutes";
-
-
+import { WishlistProvider } from "./context/WishlistContext";
 
 function App() {
   return (
     <div className="App dark:bg-slate-800 overflow-hidden">
-      <Header />
-      <Approutes />
-      <Footer />
+      <WishlistProvider>
+        <Header />
+        <Approutes />
+        <Footer />
+      </WishlistProvider>
     </div>
   );
 }
